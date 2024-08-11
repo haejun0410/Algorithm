7a38d515ed54abbdaed8acb30b742ca3a3983bb1
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2024-08-11 22:53:27 +0900

    [Bronze IV] Title: 영수증, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,13 @@
+x = int(input())
+n = int(input())
+
+result = 0
+
+for _ in range(n):
+    a,b = map(int,input().split())
+    result += a*b
+
+if x == result:
+    print("Yes")
+else:
+    print("No")
