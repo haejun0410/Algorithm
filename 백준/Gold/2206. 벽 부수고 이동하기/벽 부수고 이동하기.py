e1ef7e3d458c7c210869c82eb0f7300db4f40022
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2024-08-21 20:35:53 +0900

    Update 벽 부수고 이동하기.py

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -11,14 +11,12 @@ def check(a, b, n, m):
     return 0 <= a < n and 0 <= b < m
 
 def bfs():
-    # visited 배열을 3차원 배열로 변경하여 벽을 부수지 않은 경우와 부순 경우를 구분
     visited = [[[False] * 2 for _ in range(m)] for _ in range(n)]
     queue = deque([(0, 0, 0, 1)])  # x, y, 벽 부쉈는지 여부, 현재 거리
     
     while queue:
         x, y, broken, dist = queue.popleft()
-        
-        # 목적지에 도달하면 거리 반환
+        print(x,y,broken,dist)
         if x == n - 1 and y == m - 1:
             return dist
         
@@ -31,10 +29,8 @@ def bfs():
                     visited[nx][ny][broken] = True
                     queue.append((nx, ny, broken, dist + 1))
                 
-                elif map[nx][ny] == '1' and not broken:  # 벽을 아직 부수지 않은 경우
+                elif map[nx][ny] == '1' and not broken:
                     visited[nx][ny][1] = True
                     queue.append((nx, ny, 1, dist + 1))
-    
     return -1
-
 print(bfs())
