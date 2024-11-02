540b91665ede1600bf0a6446e669ba7c55fe7d2a
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2024-11-02 16:54:34 +0900

    [Silver III] Title: N과 M (4), Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,12 +1,16 @@
-import itertools
-
 n,m = map(int,input().split())
 
-num_list = [i for i in range(1,n+1)]
+def recursion(number):
 
-npr = itertools.combinations_with_replacement(num_list, m)
+    if number == m:
+        print(*arr)
+        return
 
-for i in list(npr):
-    for j in i:
-        print(j, end = " ")
-    print()
+    for i in range(1,n+1):
+        if arr and i < arr[-1]:
+            continue
+        arr.append(i)
+        recursion(number+1)
+        arr.pop()
+arr = []
+recursion(0)
