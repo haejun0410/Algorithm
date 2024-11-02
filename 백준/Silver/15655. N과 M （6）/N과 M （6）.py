1cbb9fc20c0b93b04562fdaf2638a9634cb54946
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2024-11-02 17:01:45 +0900

    [Silver III] Title: N과 M (6), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,19 @@
+n,m = map(int,input().split())
+
+def recursion(number):
+
+    if number == m:
+        print(*arr)
+        return
+
+    for i in range(0,n):
+        num = num_list[i]
+        if arr and num <= arr[-1]:
+            continue
+        arr.append(num_list[i])
+        recursion(number+1)
+        arr.pop()
+arr = []
+num_list = list(map(int,input().split()))
+num_list.sort()
+recursion(0)
