a33754964ae39d48b0794b830f6134fb655cc24a
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2024-11-02 16:57:35 +0900

    [Silver III] Title: N과 M (5), Time: 188 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,13 +1,19 @@
-import itertools
-import sys
-
 n,m = map(int,input().split())
 
-num_list = list(map(int,sys.stdin.readline().rstrip().split()))
-num_list.sort()
-npr = itertools.permutations(num_list, m)
+def recursion(number):
 
-for i in list(npr):
-    for j in i:
-        print(j, end = " ")
-    print()
+    if number == m:
+        print(*arr)
+        return
+
+    for i in range(0,n):
+        num = num_list[i]
+        if num in arr:
+            continue
+        arr.append(num)
+        recursion(number+1)
+        arr.pop()
+arr = []
+num_list = list(map(int,input().split()))
+num_list.sort()
+recursion(0)
