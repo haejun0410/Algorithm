48d6bcbcce4e10b2e84c05679ee6b35502c13d68
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2024-11-04 10:52:17 +0900

    [Silver II] Title: 예산, Time: 72 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.py
@@ -0,0 +1,18 @@
+N = int(input())
+lst = list(map(int, input().split()))
+M = int(input())
+
+result = 0
+start, end = 1, max(lst) 
+
+while start <= end:
+    mid = (start + end) // 2 
+    total = 0 
+    for l in lst:
+        total += min(l, mid)
+    if total <= M:
+        result = mid
+        start = mid + 1
+    else: 
+        end = mid - 1
+print(result)
