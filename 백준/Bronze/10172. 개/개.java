8a503364e66084b147035f9bd4c732a353fb7c41
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-01-15 20:55:53 +0900

    [Bronze V] Title: 개, Time: 172 ms, Memory: 17300 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|"); 
+    }
+}
