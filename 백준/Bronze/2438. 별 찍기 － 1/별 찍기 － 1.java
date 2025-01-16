a1a2a03ac10856a8981ed4e56dc2c034d1acd07b
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-01-16 12:47:40 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 248 ms, Memory: 18152 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        n = sc.nextInt();
+
+        for (int i=1; i< n+1; i++) {
+            for (int j=0; j<i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
