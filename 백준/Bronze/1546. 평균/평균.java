fbe439ad255045c28d1d42b342fccc53e6ed5c1b
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-04-14 13:44:51 +0900

    [Bronze I] Title: 평균, Time: 116 ms, Memory: 16516 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,31 +1,29 @@
-import java.util.*;
-import java.lang.*;
 import java.io.*;
+import java.util.*;
 
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n;
-        n = sc.nextInt();
-        double[] arr = new double[n];
-
-
-        double max = 0;
-        
-        for (int i=0; i<n; i++) {
-            arr[i] = sc.nextDouble();
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-        }
-
-        double answer = 0;
-        for (int i=0; i<n; i++) {
-            arr[i] = (arr[i] / max) * 100;
-            answer += arr[i];
-        }
-        System.out.println(answer / n);
-
-    }
+public class Main {
+    public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(br.readLine());
+      
+      Double [] scores = new Double[n];
+      
+      StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+      
+      for (int i=0; i<n; i++) {
+        scores[i] = Double.parseDouble(st.nextToken());
+      }
+      Arrays.sort(scores);
+      
+      Double maxScore = scores[n-1];
+      
+      Double sum = 0.0;
+      
+      for (int i=0; i<n; i++) {
+        scores[i] = (scores[i] / maxScore)*100;
+        sum += scores[i];
+      }
+      
+      System.out.println(sum/n);
+  }
 }
