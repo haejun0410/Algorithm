e2812f9bfd55484b9a4eede2f7a411273dd6ccc3
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-04-16 12:33:39 +0900

    [Bronze II] Title: 농구 경기, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[26];
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<num; i++) {
+            arr[br.readLine().charAt(0)-'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i] > 4) {
+                sb.append((char)(i+'a'));
+            }
+        }
+
+        if (sb.toString().equals("")) {
+            System.out.println("PREDAJA");
+        }
+        else {
+            System.out.println(sb);
+        }
+    }
+}
