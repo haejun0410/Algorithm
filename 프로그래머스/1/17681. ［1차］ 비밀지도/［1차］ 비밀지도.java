f019530fddc81bf4ad9eda27fcb09948123500a7
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-04-26 17:26:45 +0900

    [level 1] Title: [1차] 비밀지도, Time: 9.02 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            int combined = arr1[i] | arr2[i];
+            
+            String binaryString = String.format("%" + n + "s", Integer.toBinaryString(combined));
+            
+            binaryString = binaryString.replace('1', '#').replace('0', ' ');
+            
+            answer[i] = binaryString;
+        }
+
+        return answer;
+    }
+}
