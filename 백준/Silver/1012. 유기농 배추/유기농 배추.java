5abe21bf6267038102096229056525c002957a53
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-05-28 21:45:57 +0900

    [Silver II] Title: 유기농 배추, Time: 136 ms, Memory: 15896 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -3,89 +3,69 @@ import java.util.*;
 
 public class Main {
 
-    static int[][] map;
-    static boolean[][] visited;
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, -1, 0, 1};
 
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
-
+    static int[][] board;
     static int n,m;
+    static boolean[][] visited;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int t = Integer.parseInt(br.readLine());
+        
+        int T = Integer.parseInt(br.readLine());
 
-        for (int testcase=0; testcase<t; testcase++) {
-            st = new StringTokenizer(br.readLine());
+        for(int t=0; t<T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
             m = Integer.parseInt(st.nextToken());
             n = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
 
-            map = new int[n][m];
+            board = new int[n][m];
             visited = new boolean[n][m];
 
-            for (int i=0; i<k; i++) {
+            for(int i=0; i<k; i++) {
                 st = new StringTokenizer(br.readLine());
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
-
-                map[y][x] = 1;
+                board[y][x] = 1;
             }
-            int count = 0;
-            for (int i=0; i<n; i++) {
-                for (int j=0; j<m; j++) {
-                    if (map[i][j] == 1 && !visited[i][j]) {
-                        count++;
-                        dfs(i,j);
+
+            int cnt = 0;
+            for(int i=0; i<n; i++) {
+                for(int j=0; j<m; j++) {
+                    if (!visited[i][j] && board[i][j] == 1) {
+                        bfs(i, j);
+                        cnt++;
                     }
                 }
             }
 
-            System.out.println(count);
-        }
-    }
+            System.out.println(cnt);
 
-    public static class Point  {
-        int x, y;
-
-        public Point(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
-
-        public int getX() {
-            return this.x;
-        }
-
-        public int getY() {
-            return this.y;
-        }
-
-        public void setX(int x) {
-            this.x = x;
-        }
-
-        public void setY(int y) {
-            this.y = y;
         }
 
     }
 
-    public static void dfs(int y, int x) {
+    public static void bfs(int y, int x) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[] {y, x});
         visited[y][x] = true;
+        while(!queue.isEmpty()) {
+            int[] current = queue.poll();
 
-        for (int i=0; i<4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
+            for(int i=0; i<4; i++) {
+                int ny = current[0] + dy[i];
+                int nx = current[1] + dx[i];
 
-            if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
-                if (map[ny][nx] == 1 && !visited[ny][nx]) {
-                    dfs(ny, nx);
+                if (ny >= 0 && ny < n && nx >= 0 && nx < m) {
+                    if (!visited[ny][nx] && board[ny][nx] == 1) {
+                        queue.offer(new int[] {ny, nx});
+                        visited[ny][nx] = true;
+                    }
                 }
             }
         }
     }
-
 }
