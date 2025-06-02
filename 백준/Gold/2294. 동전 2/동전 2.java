6c751c18e40fed274d02a6fdcacb66b16928f69f
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-06-02 15:19:56 +0900

    [Gold V] Title: 동전 2, Time: 120 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -3,35 +3,31 @@ import java.util.*;
 
 public class Main {
 
-    static Integer[] coins;
-    static int n,k;
-    static int[] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
 
-        coins = new Integer[n];
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
+        int[] coins = new int[n];
         for(int i=0; i<n; i++) {
             coins[i] = Integer.parseInt(br.readLine());
         }
 
-        dp = new int[10001];
-        Arrays.fill(dp, 10001);
+        Arrays.sort(coins);
 
+        int[] dp = new int[k+1];
+        Arrays.fill(dp, 100001);
         dp[0] = 0;
 
         for(int i=0; i<n; i++) {
-            for (int j= coins[i]; j<= k; j++) {
-                dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+            for(int j=coins[i]; j<=k; j++) {
+                dp[j] = Math.min(dp[j], dp[j-coins[i]] + 1);
             }
         }
 
-        System.out.println(dp[k] == 10001 ? -1 : dp[k]);
+        System.out.println(dp[k] == 100001 ? -1 : dp[k]);
     }
-
 }
