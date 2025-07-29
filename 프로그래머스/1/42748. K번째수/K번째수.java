0675a2502e614196e3b6658897321b0aa82c6677
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-07-29 19:47:29 +0900

    [level 1] Title: K번째수, Time: 3.12 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,24 +2,15 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
         
-        for (int t = 0; t<commands.length; t++) {
-            int s = commands[t][0];
-            int e = commands[t][1];
-            int k = commands[t][2];
-            
-            int cnt = 0;
-            ArrayList<Integer> subArr = new ArrayList<>();
-            for (int i=s-1; i<e; i++) {
-                subArr.add(array[i]);
-            }
-            
-            Collections.sort(subArr);
-            answer[t] = subArr.get(k-1);
-            
+        ArrayList<Integer> answer = new ArrayList<>();
+        for(int[] command : commands) {
+            int[] sliced = Arrays.copyOfRange(array, command[0]-1, command[1]);
+            Arrays.sort(sliced);
+            answer.add(sliced[command[2]-1]);
         }
         
-        return answer;
+        int[] result = answer.stream().mapToInt(Integer::intValue).toArray();
+        return result;
     }
 }
