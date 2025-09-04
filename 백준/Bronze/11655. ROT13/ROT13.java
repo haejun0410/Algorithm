cf2d1d9de4bc9d57ad30f3882b1f6533486dd62c
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-09-04 19:42:41 +0900

    [Bronze I] Title: ROT13, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.java
+++ 백준/Bronze/11655. ROT13/ROT13.java
@@ -1,27 +1,28 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String word = br.readLine();
+        String sentence = br.readLine();
 
         StringBuilder sb = new StringBuilder();
 
-        for (int i=0; i<word.length(); i++) {
-            Character c = word.charAt(i);
-            
-            if (c >= 'A' && c <= 'Z') {
-                sb.append((char) ((c - 'A' + 13) % 26 + 'A'));
+        for(int i=0; i<sentence.length(); i++){
+            Character c = sentence.charAt(i);
+
+            if (c >= 'a' && c <= 'z') {
+                sb.append((char)((c - 'a' + 13) % 26 + 'a'));
             }
-            else if ( c >= 'a' && c <= 'z') {
-                sb.append((char) ((c - 'a' + 13) % 26 + 'a'));
+            else if (c >= 'A' && c <= 'Z') {
+                sb.append((char)((c - 'A' + 13) % 26 + 'A'));
             }
             else {
                 sb.append(c);
             }
         }
 
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
