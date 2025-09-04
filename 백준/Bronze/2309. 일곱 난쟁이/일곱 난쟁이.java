2b464b8db73eb25572f91864cdeebe2db1016927
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-09-04 12:00:41 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -2,34 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] arr = new int[9];
-        int sum = 0;
-        for (int i=0; i<9; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            sum += arr[i];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] heights = new int[9];
+
+        int heightSum = 0;
+
+        for(int i=0; i<9; i++) {
+            heights[i] = Integer.parseInt(br.readLine());
+            heightSum += heights[i];
         }
 
-        int diff = sum-100;
-        Arrays.sort(arr);
-        StringBuilder sb = new StringBuilder();
-        
+        int targetSum = heightSum - 100;
+
+        int exclude1 = -1;
+        int exclude2 = -1;
+
+        Arrays.sort(heights);
+
         outer:
-        for (int i=0; i<8; i++) {
-            for (int j=i+1; j<9; j++) {
-                if (arr[i] + arr[j] == diff) {
-                    for(int k=0; k<9; k++) {
-                        if (k!=i && k!= j) {
-                            sb.append(arr[k]).append("\n");
-                        }
-                    }
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
+                if (heights[i] + heights[j] == targetSum) {
+                    exclude1 = i;
+                    exclude2 = j;
                     break outer;
                 }
             }
         }
 
-        System.out.println(sb);
+        for(int i=0; i<9; i++) {
+            if (i != exclude1 && i != exclude2) {
+                System.out.println(heights[i]);
+            }
+        }
     }
+
 }
