193d6dcc56ac3d73a7fe5435a4db1598e8281217
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-09-05 20:19:52 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 452 ms, Memory: 57256 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -4,33 +4,35 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
+
+        HashMap<String, String> nameToNum = new HashMap<>();
+        HashMap<String, String> numToName = new HashMap<>();
+
+        for(int i=1; i<n+1; i++) {
+            String name = br.readLine();
+            nameToNum.put(name, String.valueOf(i));
+            numToName.put(String.valueOf(i), name);
+        }
+
+
         StringBuilder sb = new StringBuilder();
 
-        HashMap<Integer, String> hash1 = new HashMap<>();
-        HashMap<String, Integer> hash2 = new HashMap<>();
-
-        for (int i=0; i<n; i++) {
-            String s = br.readLine();
-            hash1.put(i+1, s);
-            hash2.put(s, i+1);
-        }
-
-        for (int i=0; i<m; i++) {
-            String s = br.readLine();
-            if (49 <= s.charAt(0) && s.charAt(0) <= 57) {
-                sb.append(hash1.get(Integer.parseInt(s))).append("\n");
+        for(int i=0; i<m; i++) {
+            String target = br.readLine();
+            if (Character.isAlphabetic(target.charAt(0))) {
+                sb.append(nameToNum.get(target)).append("\n");
             }
             else {
-                sb.append(hash2.get(s)).append("\n");
+                sb.append(numToName.get(target)).append("\n");
             }
         }
 
-        System.out.println(sb);
-
+        System.out.println(sb.toString());
     }
 }
