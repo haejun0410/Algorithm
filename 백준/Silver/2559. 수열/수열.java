bf646f47c1ba0b3d31726157a29e5e37b551d061
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-09-05 17:47:22 +0900

    [Silver III] Title: 수열, Time: 240 ms, Memory: 24396 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -6,28 +6,29 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n];
+        int[] temperatures = new int[n];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<n; i++) {
+            temperatures[i] = Integer.parseInt(st.nextToken());
         }
 
-        int sum = 0;
-        for (int i = 0; i < k; i++) {
-            sum += arr[i];
+        int[] prefix = new int[n+1];
+        for(int i=1; i<n+1; i++) {
+            prefix[i] = prefix[i-1] + temperatures[i-1];
         }
 
-        int max = sum;
+        int maxSum = Integer.MIN_VALUE;
 
-        for (int i = k; i < n; i++) {
-            sum = sum - arr[i - k] + arr[i];
-            max = Math.max(max, sum);
+        for(int i=k; i<=n; i++) {
+            int sum = prefix[i] - prefix[i-k];
+            maxSum = Math.max(maxSum, sum);
         }
 
-        System.out.println(max);
+        System.out.println(maxSum);
     }
 }
