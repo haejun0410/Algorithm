ff7975388c11dbba0d16c8865f7e030d9d58368b
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-09-09 13:56:19 +0900

    [Silver I] Title: 영역 구하기, Time: 116 ms, Memory: 15060 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -3,77 +3,82 @@ import java.util.*;
 
 public class Main {
 
-    static int m, n;
-    static int[][] map;
-    static boolean[][] visited;
-
     static int[] dy = {-1, 0, 1, 0};
     static int[] dx = {0, -1, 0, 1};
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        map = new int[n][m];
+        int[][] board = new int[n+1][m+1];
 
-        for (int i=0; i<k; i++) {
+        for(int i=0; i<k; i++) {
             st = new StringTokenizer(br.readLine());
+
             int y1 = Integer.parseInt(st.nextToken());
             int x1 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
 
-            for (int y = y1; y<=y2-1; y++) {
-                for (int x=x1; x<=x2-1; x++) {
-                    map[y][x]++;
+            board[y1][x1]++;
+            board[y1][x2]--;
+            board[y2][x1]--;
+            board[y2][x2]++;
+
+        }
+
+        for(int i=0; i<n; i++) {
+            for(int j=1; j<m; j++) {
+                board[i][j] += board[i][j-1];
+            }
+        }
+
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                board[i][j] += board[i-1][j];
+            }
+        }
+
+        int cnt = 0;
+        boolean[][] visited = new boolean[n][m];
+        Queue<int[]> queue = new ArrayDeque<>();
+        ArrayList<Integer> areaList = new ArrayList<>();
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if (board[i][j] == 0 && !visited[i][j]) {
+                    visited[i][j] = true;
+                    queue.offer(new int[] {i, j});
+                    cnt++;
+                    int area = 0;
+                    while(!queue.isEmpty()) {
+                        int[] curr = queue.poll();
+                        area++;
+                        for(int idx=0; idx<4; idx++) {
+                            int ny = curr[0] + dy[idx];
+                            int nx = curr[1] + dx[idx];
+
+                            if (ny >= 0 && ny < n && nx >= 0 && nx < m && !visited[ny][nx] && board[ny][nx] == 0) {
+                                visited[ny][nx] = true;
+                                queue.offer(new int[] {ny, nx});
+                            }
+                        }
+                    }
+                    areaList.add(area);
+
                 }
             }
         }
 
-        visited = new boolean[n][m];
-
-        int count = 0;
-        List<Integer> sizes = new ArrayList<>();
-
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                if (map[i][j] == 0 && !visited[i][j]) {
-                    count++;
-                    int size = dfs(i, j);
-                    sizes.add(size);
-                }
-            }
+        Collections.sort(areaList);
+        System.out.println(cnt);
+        for(Integer area : areaList) {
+            System.out.print(area + " ");
         }
-
-        System.out.println(count);
-        Collections.sort(sizes);
-
-        for (int size : sizes) {
-            System.out.print(size + " ");
-        }
-    }
-
-    public static int dfs(int y, int x) {
-        visited[y][x] = true;
-        int count = 1;
-
-        for (int i=0; i<4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-
-            if (ny >= 0 && ny < n && nx >= 0 && nx < m) {
-                if (map[ny][nx] == 0 && !visited[ny][nx]) {
-                    count += dfs(ny, nx);
-                }
-            }
-        }
-
-        return count;
     }
 }
