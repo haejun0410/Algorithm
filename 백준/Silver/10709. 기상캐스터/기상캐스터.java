f7c789eaccbd1fa83f1dc631cf5a3950f35fbbda
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-09-10 12:51:44 +0900

    [Silver V] Title: 기상캐스터, Time: 112 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.java
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.java
@@ -2,59 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int h = Integer.parseInt(st.nextToken());
-		int w = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		Character[][] map = new Character[h][w];
-		int[][] time = new int[h][w];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
 
-		for (int i=0; i<h; i++) {
-			Arrays.fill(time[i], -1);
-		}
+        int[][] board = new int[h][w];
 
-		for (int i=0; i<h; i++) {
-			String arr = br.readLine();
+        for(int i=0; i<h; i++) {
+            String line = br.readLine();
+            for(int j=0; j<w; j++) {
+                if (line.charAt(j) == 'c') {
+                    board[i][j] = 0;
+                }
+                else {
+                    board[i][j] = -1;
+                }
+            }
+        }
 
-			for (int j=0; j<w; j++) {
-				map[i][j] = arr.charAt(j);
-				if (arr.charAt(j) == 'c') {
-					time[i][j] = 0;
-				}
-			}
-		}
+        for(int i=0; i<h; i++) {
+            for(int j=1; j<w; j++) {
+                if(board[i][j-1] != -1 && board[i][j] == -1) {
+                    board[i][j] = board[i][j-1] + 1;
+                }
+            }
+        }
 
-		for (int r=0; r<w-1; r++) {
-			for (int i=0; i<h; i++) {
-				for (int j=w-1; j>0; j--) {
-					map[i][j] = map[i][j-1];
-				}
-				map[i][0] = '.';
-			}
+        StringBuilder sb = new StringBuilder();
 
-			for (int i=0; i<h; i++) {
-				for (int j=0; j<w; j++) {
-					if (time[i][j] == -1 && map[i][j] == 'c') {
-						time[i][j] = time[i][j-1] + 1;
-					}
-				}
-			}
-		}
+        for(int i=0; i<h; i++) {
+            for(int j=0; j<w; j++) {
+                sb.append(board[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
 
-		StringBuilder sb = new StringBuilder();
-		for (int i=0; i<h; i++) {
-			for (int j=0; j<w; j++) {
-				sb.append(time[i][j]).append(" ");
-			}
-			sb.append("\n");
-		}
+        System.out.println(sb.toString());
+    }
 
-		System.out.println(sb);
-	}
-	
 }
