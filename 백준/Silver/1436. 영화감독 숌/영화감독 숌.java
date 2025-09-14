0bec8ddb61fa5ab9268553e9581fb57ba43115c6
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-09-14 13:13:31 +0900

    [Silver V] Title: 영화감독 숌, Time: 264 ms, Memory: 85324 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,20 +1,27 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int n = Integer.parseInt(br.readLine());
-      
-      int num = 666;
-      int count = 1;
-      
-      while (count != n) {
-        num++;
-        if (String.valueOf(num).contains("666")) {
-          count++;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int num = 666;
+        int count = 0;
+
+        for(;;) {
+            if (String.valueOf(num).contains("666")) {
+                count++;
+            }
+            if (count == n) {
+                break;
+            }
+            num++;
         }
-      }
-      System.out.println(num);
-  }
+
+        System.out.println(num);
+    }
+
 }
