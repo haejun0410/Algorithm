fd84ee4eb65ccb7c882810eecd59b1bc6404d754
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-09-15 12:33:34 +0900

    [Gold IV] Title: 오큰수, Time: 820 ms, Memory: 147992 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -6,35 +6,43 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
         int[] arr = new int[n];
-        int[] answer = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] result = new int[n];
 
-        for (int i = 0; i < n; i++) {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        Deque<Integer> stack = new ArrayDeque<>();
 
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
-                stack.pop();
-            }
-
+        for(int i=n-1; i>=0; i--) {
             if (stack.isEmpty()) {
-                answer[i] = -1;
-            } else {
-                answer[i] = stack.peek();
+                result[i] = -1;
+                stack.push(arr[i]);
             }
+            else {
+                while(!stack.isEmpty() && arr[i] >= stack.peek()) {
+                    stack.pop();
+                }
+                if (stack.isEmpty()) {
+                    result[i] = -1;
+                }
+                else {
+                    result[i] = stack.peek();
+                }
 
-            stack.push(arr[i]);
+                stack.push(arr[i]);
+            }
         }
 
         StringBuilder sb = new StringBuilder();
-		for (int a : answer) {
-			sb.append(a).append(" ");
-		}
-		System.out.println(sb);
+
+        for(int num: result) {
+            sb.append(num).append(" ");
+        }
+
+        System.out.println(sb.toString());
+
+
     }
 }
