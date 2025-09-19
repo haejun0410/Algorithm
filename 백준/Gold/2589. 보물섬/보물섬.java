deb3cde1fa9ec5ed3df8a5d339e72edb0c50465e
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-09-19 20:41:52 +0900

    [Gold V] Title: 보물섬, Time: 488 ms, Memory: 191188 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.java
+++ 백준/Gold/2589. 보물섬/보물섬.java
@@ -2,79 +2,62 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int n,m;
 
-	static int[] dy = {-1, 0, 1, 0};
-	static int[] dx = {0, -1, 0, 1};
-	static Character[][] map;
+    static int n;
+    static int m;
+    static char[][] board;
+
+    static int length = Integer.MIN_VALUE;
+
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, -1, 0, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+        board = new char[n][m];
 
-		map = new Character[n][m];
+        for(int i=0; i<n; i++) {
+            String line = br.readLine();
+            for(int j=0; j<m; j++) {
+                board[i][j] = line.charAt(j);
+            }
+        }
 
-		for (int i=0; i<n; i++) {
-			String line = br.readLine();
-			for(int j=0; j<m; j++) {
-				map[i][j] = line.charAt(j);
-			}
-		}
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if (board[i][j] == 'L') {
+                    bfs(i, j);
+                }
+            }
+        }
 
-		int maxDistance = -1;
+        System.out.println(length - 1);
+    }
 
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<m; j++) {
-				if (map[i][j] == 'L') {
-					int temp = bfs(i, j);
-					maxDistance = Math.max(temp, maxDistance);
-				}
-			}
-		}
+    public static void bfs(int y, int x) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[] {y, x});
+        int[][] visited = new int[n][m];
+        visited[y][x] = 1;
 
-		System.out.println(maxDistance);
-	}
 
-	public static int bfs(int y, int x) {
-		int answer = -1;
-		int[][] visited = new int[n][m];
+        while(!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            for(int i=0; i<4; i++) {
+                int ny = curr[0] + dy[i];
+                int nx = curr[1] + dx[i];
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				visited[i][j] = -1;
-			}
-		}
-		Queue<node> queue = new LinkedList<>();
-		queue.offer(new node(y, x));
-		visited[y][x] = 0;
-		while (!queue.isEmpty()) {
-			node current = queue.poll();
-			for(int i=0; i<4; i++) {
-				int ny = current.y + dy[i];
-				int nx = current.x + dx[i];
+                if (ny >= 0 && ny < n && nx >= 0 && nx < m && board[ny][nx] == 'L' && visited[ny][nx] == 0) {
+                    queue.offer(new int[] {ny, nx});
+                    visited[ny][nx] = visited[curr[0]][curr[1]] + 1;
+                    length = Math.max(length, visited[curr[0]][curr[1]] + 1);
+                }
+            }
+        }
 
-				if (ny >= 0 && ny < n && nx >= 0 && nx < m) {
-					if (visited[ny][nx] == -1 && map[ny][nx] == 'L') {
-						visited[ny][nx] = visited[current.y][current.x] + 1;
-						answer = Math.max(visited[ny][nx], answer);
-						queue.offer(new node(ny, nx));
-					}
-				}
-			}
-		}
-
-		return answer;
-	}
-
-	public static class node {
-		int y, x;
-
-		public node (int y, int x) {
-			this.y = y;
-			this.x = x;
-		}
-	}
+    }
 }
