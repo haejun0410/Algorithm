38000a6e9421c52788da8b8950c678bf04c5b2f3
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-09-26 21:45:23 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.09 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 비밀지도 - 17681 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681?language=java) 
 
 ### 성능 요약
 
-메모리: 88.2 MB, 시간: 9.02 ms
+메모리: 78.6 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 26일 17:26:42
+2025년 09월 26일 21:45:20
 
 ### 문제 설명
 
--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,17 +1,20 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-
-        for (int i = 0; i < n; i++) {
-            int combined = arr1[i] | arr2[i];
-            
-            String binaryString = String.format("%" + n + "s", Integer.toBinaryString(combined));
-            
-            binaryString = binaryString.replace('1', '#').replace('0', ' ');
-            
-            answer[i] = binaryString;
+        for(int i=0; i<n; i++) {
+            int map = arr1[i] | arr2[i];
+            StringBuilder sb = new StringBuilder();
+            for(int j=1; j<1<<n; j*=2) {
+                if ((map & j) >= 1) {
+                    sb.append("#");
+                }
+                else {
+                    sb.append(" ");
+                }
+            }
+            answer[i] = sb.reverse().toString();
         }
-
+        
         return answer;
     }
 }
