e674b85ece81077f23672102156f5e8a4cf98405
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-09-26 12:39:39 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 108 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -3,42 +3,43 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        int[] scvs = new int[] {0, 0, 0};
-        int[][] attack = {
+    static int[][] attack = {
             {9, 3, 1},
             {9, 1, 3},
-            {3, 9, 1},
-            {3, 1, 9},
             {1, 3, 9},
-            {1, 9, 3}
-        };
+            {1, 9, 3},
+            {3, 1, 9},
+            {3, 9, 1}
 
+    };
+
+    static int[] scvs = new int[] {0, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+
+        for(int i=0; i<n; i++) {
             scvs[i] = Integer.parseInt(st.nextToken());
         }
-
-        boolean[][][] visited = new boolean[61][61][61];
-        Queue<point> queue = new LinkedList<>();
-        queue.offer(new point(scvs[0], scvs[1], scvs[2]));
-        visited[scvs[0]][scvs[1]][scvs[2]] = true;
+        boolean[][][] visited =new boolean[61][61][61];
 
         int time = 0;
-        while (!queue.isEmpty()) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[] {scvs[0], scvs[1], scvs[2]});
+
+        while(!queue.isEmpty()) {
             int size = queue.size();
             time++;
 
-            for (int i = 0; i < size; i++) {
-                point cur = queue.poll();
-
-                for (int[] atk : attack) {
-                    int nx = Math.max(0, cur.x - atk[0]);
-                    int ny = Math.max(0, cur.y - atk[1]);
-                    int nz = Math.max(0, cur.z - atk[2]);
+            for(int i=0; i<size; i++) {
+                int[] curr = queue.poll();
+                for(int[] atk : attack) {
+                    int nx = Math.max(0, curr[0] - atk[0]);
+                    int ny = Math.max(0, curr[1] - atk[1]);
+                    int nz = Math.max(0, curr[2] - atk[2]);
 
                     if (nx == 0 && ny == 0 && nz == 0) {
                         System.out.println(time);
@@ -47,19 +48,15 @@ public class Main {
 
                     if (!visited[nx][ny][nz]) {
                         visited[nx][ny][nz] = true;
-                        queue.offer(new point(nx, ny, nz));
+                        queue.offer(new int[] {nx, ny, nz});
                     }
                 }
             }
+
+
         }
     }
 
-    static class point {
-        int x, y, z;
-        point(int x, int y, int z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-    }
+
+
 }
