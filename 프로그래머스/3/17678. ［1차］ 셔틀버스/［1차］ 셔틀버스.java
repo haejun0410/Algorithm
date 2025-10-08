58beec5af6821ac4359094fc272f118b5aedd5cc
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-10-08 21:34:37 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 14.39 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int n, int t, int m, String[] timetable) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(String table : timetable) {
+            int time = Integer.parseInt(table.substring(0, 2)) * 60 + Integer.parseInt(table.substring(3));
+            pq.add(time);
+        }
+        
+        int startTime = 9 * 60;
+        int lastTime = 0;
+        
+        int total = 0;
+        
+        for(int i=0; i<n; i++) {
+            total = 0;
+            while(!pq.isEmpty()) {
+                int currentTime = pq.peek();
+                
+                if(currentTime <= startTime && total < m) {
+                    pq.poll();
+                    total++;
+                }
+                else break;
+                lastTime = currentTime - 1;
+            }
+            
+            startTime += t;
+        }
+        
+        if (total < m) {
+            lastTime = startTime - t;
+        }
+        
+        String hour = String.format("%02d", lastTime / 60);
+        String minute = String.format("%02d", lastTime % 60);
+        
+        return hour + ":" + minute;
+    }
+}
