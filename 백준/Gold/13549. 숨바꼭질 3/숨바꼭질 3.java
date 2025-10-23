44446508426b31786b33d91a4b1f937ca0f05216
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-10-23 13:02:17 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 140 ms, Memory: 16524 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -4,36 +4,36 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-		int[] visited = new int[100001];
-		Arrays.fill(visited, -1); 
+        int[] visited = new int[100001];
+        Arrays.fill(visited, -1);
 
-		Deque<Integer> deque = new ArrayDeque<>();
-		deque.offer(n);
-		visited[n] = 0;
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.offer(n);
+        visited[n] = 0;
 
-		while (!deque.isEmpty()) {
-			int current = deque.poll();
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
 
-			if (current * 2 <= 100000 && visited[current * 2] == -1) {
-				visited[current * 2] = visited[current];
-				deque.offerFirst(current * 2); 
-			}
+            if (current * 2 <= 100000 && visited[current * 2] == -1) {
+                visited[current * 2] = visited[current];
+                queue.offerFirst(current * 2);
+            }
 
-			if (current - 1 >= 0 && visited[current - 1] == -1) {
-				visited[current - 1] = visited[current] + 1;
-				deque.offerLast(current - 1); 
-			}
+            if (current - 1 >= 0 && visited[current - 1] == -1) {
+                visited[current - 1] = visited[current] + 1;
+                queue.offerLast(current - 1);
+            }
 
-			if (current + 1 <= 100000 && visited[current + 1] == -1) {
-				visited[current + 1] = visited[current] + 1;
-				deque.offerLast(current + 1);
-			}
-		}
+            if (current + 1 <= 100000 && visited[current + 1] == -1) {
+                visited[current + 1] = visited[current] + 1;
+                queue.offerLast(current + 1);
+            }
+        }
 
-		System.out.println(visited[m]);
+        System.out.println(visited[m]);
     }
 }
