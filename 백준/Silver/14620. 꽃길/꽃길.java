14b20a4a4a0ea75babbcea77c1d37a20714736cd
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-11-14 20:25:36 +0900

    [Silver II] Title: 꽃길, Time: 192 ms, Memory: 18764 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.java
+++ 백준/Silver/14620. 꽃길/꽃길.java
@@ -3,91 +3,62 @@ import java.util.*;
 
 public class Main {
 
-	static int n;
-	static int[][] garden;
-	static int[][] seed;
-
-	static int[] dy = {0, -1, 0, 1, 0};
-	static int[] dx = {0, 0, -1, 0, 1};
-
-	static int cost = 2147483647;
+    static int n;
+    static int[][] board;
+    static boolean[][] planted;
+    static int answer = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-		garden = new int[n][n];
-		seed = new int[n][n];
 
-		for (int i=0; i<n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j=0; j<n; j++) {
-				garden[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+        board = new int[n][n];
+        planted = new boolean[n][n];
 
-		backtracking(0);
+        for(int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-		System.out.println(cost);
+        backtracking(0);
 
+        System.out.println(answer);
     }
 
-	public static void calculate() {
-		int answer = 0;
+    public static void backtracking(int count) {
+        if (count == 3) {
+            answer = Math.min(answer, calculate());
+            return;
+        }
 
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<n; j++) {
-				if (seed[i][j] == 1) {
-					answer += garden[i][j];
-				}
-			}
-		}
-		cost = Math.min(cost, answer);
+        for(int i=1; i<n-1; i++) {
+            for(int j=1; j<n-1; j++) {
+                if (!planted[i][j]) {
+                    if (!planted[i-1][j] && !planted[i][j-1] && !planted[i+1][j] && !planted[i][j+1]) {
+                        planted[i][j] = true; planted[i-1][j] = true; planted[i][j-1] = true; planted[i+1][j] = true; planted[i][j+1] = true;
+                        backtracking(count + 1);
+                        planted[i][j] = false; planted[i-1][j] = false; planted[i][j-1] = false; planted[i+1][j] = false; planted[i][j+1] = false;
+                    }
+                }
+            }
+        }
+    }
 
-	}
+    public static int calculate() {
+        int sum = 0;
 
-	public static void backtracking(int count) {
-		if (count == 3) {
-			calculate();
-			return;
-		}
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if (planted[i][j]) {
+                    sum += board[i][j];
+                }
+            }
+        }
 
-		for(int i=1; i<n-1; i++) {
-			for(int j=1; j<n-1; j++) {
-				if (seed[i][j] == 0) {
-					if (!makeFlower(i, j)) {
-						unFlower(i, j);
-						continue;
-					}
-					backtracking(count+1);
-					unFlower(i, j);
-				}
-			}
-		}
-	}
-
-	public static boolean makeFlower(int y, int x) {
-		boolean flag = true;
-		for(int i=0; i<5; i++) {
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-				
-			seed[ny][nx]++;
-			if (seed[ny][nx] > 1) {
-				flag = false;
-			}
-		}
-		
-
-		return flag;
-	}
-
-	public static void unFlower(int y, int x) {
-		for(int i=0; i<5; i++) {
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			seed[ny][nx]--;
-		}
-	}
-
+        return sum;
+    }
 
 }
