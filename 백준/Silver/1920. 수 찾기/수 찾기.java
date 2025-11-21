e2d0c2fc8bab809c7fe887c0d02e301aeec16ab8
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-11-21 10:22:43 +0900

    [Silver IV] Title: 수 찾기, Time: 592 ms, Memory: 44828 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,54 +1,48 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class Main{
+
     public static void main(String[] args) throws IOException{
-      
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      
-      int n = Integer.parseInt(br.readLine());
-      int[] a = new int[n];
-      
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      
-      for (int i=0; i<n; i++) {
-        a[i] = Integer.parseInt(st.nextToken());
-      }
-      
-      Arrays.sort(a);
-      
-      int m = Integer.parseInt(br.readLine());
-      
-      st = new StringTokenizer(br.readLine());
-      for (int i=0; i<m ; i++) {
-        if (find(a, Integer.parseInt(st.nextToken()))) {
-          System.out.println("1");
-        }  
-        else {
-          System.out.println("0");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            a[i] = Integer.parseInt(st.nextToken());
         }
-      }
-  }
-  
-  public static boolean find(int[] arr, int target) {
-    int start = 0;
-    int end = arr.length -1;
-    int mid = 0;
-    
-    while (start <= end) {
-      mid = (start + end) / 2;
-      
-      if (arr[mid] > target) {
-        end = mid-1;
-      }
-      else if (arr[mid] < target) {
-        start = mid+1;
-      }
-      else {
-        return true;
-      }
+        Arrays.sort(a);
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < m; i++){
+            int num = Integer.parseInt(st.nextToken());
+            sb.append(find(a, num) ? 1 : 0).append("\n");
+        }
+        System.out.println(sb);
     }
-    
-    return false;
-  }
+
+    public static boolean find(int[] arr, int x) {
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start <= end) {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == x) {
+                return true;
+            }
+
+            else if (arr[mid] > x){
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+
+        return false;
+    }
 }
