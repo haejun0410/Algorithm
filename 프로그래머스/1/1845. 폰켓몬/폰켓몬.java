669b4b714a0dc5784c7be2970f84e6201cd77944
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2025-12-30 16:54:31 +0900

    [level 1] Title: 폰켓몬, Time: 6.63 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,15 +1,15 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
+        
         for(int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
         
-        int selectNum = nums.length/2;
-        answer = Math.min(selectNum, map.keySet().size());
+        int answer = Math.min(nums.length/2, map.size());
+        
         return answer;
     }
 }
