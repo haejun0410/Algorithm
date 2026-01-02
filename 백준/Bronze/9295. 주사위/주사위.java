da2f96050eff2e5c836d4ae400f33e8b33358e62
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2026-01-02 21:24:21 +0900

    [Bronze IV] Title: 주사위, Time: 116 ms, Memory: 14560 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=1; i<=n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append("Case").append(" ").append(i).append(": ")
+                .append(a+b).append("\n");
+
+        }
+
+        System.out.println(sb);
+    }
+}
