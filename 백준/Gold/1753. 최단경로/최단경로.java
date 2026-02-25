e21d515568ef55589b73cc25ed1a1cd6365eac8d
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2026-02-25 15:26:48 +0900

    [Gold IV] Title: 최단경로, Time: 708 ms, Memory: 109804 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,70 +2,63 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    static class Node {
-        int idx;
-        int cost;
-
-        public Node(int idx, int cost) {
-            this.idx = idx;
-            this.cost = cost;
-        }
-    }
-
-    static ArrayList<Node>[] graph;
-    static boolean[] visited;
-    static int[] dist;
+    static ArrayList<ArrayList<int[]>> adj;
+    static int[] distance;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
 
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(br.readLine());
 
-        int K = Integer.parseInt(br.readLine());
-
-        graph = new ArrayList[V+1];
-        dist = new int[V+1];
-        visited = new boolean[V+1];
-
-        for(int i=1; i<=V; i++) {
-            graph[i] = new ArrayList<>();
-            dist[i] = Integer.MAX_VALUE;
+        adj = new ArrayList<>();
+        for (int i = 0; i <= v; i++) {
+            adj.add(new ArrayList<>());
         }
 
-        for(int i=0; i<E; i++) {
+        for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-
-            graph[u].add(new Node(v, w));
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            adj.get(from).add(new int[] {to, weight});
         }
 
-        dijkstra(K);
+        distance = new int[v + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
 
-        for(int i=1; i<=V; i++) {
-            System.out.println(dist[i] == Integer.MAX_VALUE ? "INF" : dist[i]);
+        dijkstra(start);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= v; i++) {
+            sb.append(distance[i] == Integer.MAX_VALUE ? "INF" : distance[i]).append("\n");
         }
+        System.out.print(sb);
     }
 
     public static void dijkstra(int start) {
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
 
-        pq.add(new Node(start, 0));
-        dist[start] = 0;
+        distance[start] = 0;
+        pq.offer(new int[] {start, 0});
 
         while (!pq.isEmpty()) {
-            Node now = pq.poll();
-            visited[now.idx] = true;
+            int[] current = pq.poll();
+            int currNode = current[0];
+            int currDist = current[1];
 
-            for (Node next : graph[now.idx]) {
-                if (!visited[next.idx] && dist[next.idx] > now.cost + next.cost) {
-                    dist[next.idx] = now.cost + next.cost;
-                    pq.add(new Node(next.idx, dist[next.idx]));
+            if (distance[currNode] < currDist) continue;
+
+            for (int[] edge : adj.get(currNode)) {
+                int nextNode = edge[0];
+                int nextWeight = edge[1];
+
+                if (distance[nextNode] > distance[currNode] + nextWeight) {
+                    distance[nextNode] = distance[currNode] + nextWeight;
+                    pq.offer(new int[] {nextNode, distance[nextNode]});
                 }
             }
         }
