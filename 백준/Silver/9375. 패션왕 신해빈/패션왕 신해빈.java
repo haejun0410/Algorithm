676d842e464132961a222e8c4cbcd247a3e4c545
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2026-03-03 17:31:45 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,35 +1,34 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int T = Integer.parseInt(br.readLine());
-
         StringBuilder sb = new StringBuilder();
 
-        for(int t=0; t<T; t++) {
-            int n = Integer.parseInt(br.readLine());
-            HashMap<String, Integer> clothes = new HashMap<>();
+        int TC = Integer.parseInt(br.readLine());
 
-            for(int i=0; i<n; i++) {
+        for (int testCase = 1; testCase <= TC; testCase++) {
+            int n = Integer.parseInt(br.readLine());
+            
+            HashMap<String, Integer> map = new HashMap<>();
+            for (int i = 0; i < n; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 String name = st.nextToken();
-                String category = st.nextToken();
+                String type = st.nextToken();
 
-                clothes.put(category, clothes.getOrDefault(category, 0) + 1);
+                map.put(type, map.getOrDefault(type, 0) + 1);
             }
 
             int answer = 1;
 
-            for(Integer count : clothes.values()) {
-                answer *= (count + 1);
+            for (int count : map.values()) {
+                answer *= (count+1);
             }
 
-            sb.append(answer - 1).append("\n");
+            sb.append(answer -1).append("\n");
         }
 
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
