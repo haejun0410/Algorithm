996312804a2a88ae25732b85b44db4d2a5073eb3
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2026-03-05 10:32:35 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 104 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -1,43 +1,45 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String s = br.readLine();
-        int[] alpha = new int[26];
+        String word = br.readLine();
+        int[] countAlphabet = new int[26];
 
-        for (int i=0; i<s.length(); i++) {
-            int idx = s.charAt(i)-'A';
-            alpha[idx]++;
+        for (int i = 0; i < word.length(); i++) {
+            countAlphabet[word.charAt(i) - 'A']++;
         }
 
-        int isOne = 0;
-        int oneIdx = 0;
-        for (int i=0; i<alpha.length; i++) {
-            if (alpha[i] % 2 != 0) {
-                isOne++;
-                oneIdx = i;
-            }
-        }
-
-        String answer = "";
+        int cntOdd = 0;
+        int index = -1;
         StringBuilder sb = new StringBuilder();
-        if (isOne>1) {
-            answer += "I'm Sorry Hansoo";
-        }
-        else {
-            for (int i=0; i<alpha.length; i++) {
-                for (int r=0; r<alpha[i]/2; r++) {
-                    sb.append((char)(i+65));
+        char odd = ' ';
+        for (int i = 0; i < 26; i++) {
+            int cnt = countAlphabet[i];
+            if (cnt != 0) {
+                char ch = (char)(i + 'A');
+                if (cnt % 2 == 1) {
+                    cntOdd++;
+                    index = i;
+                }
+
+                for (int j = 0; j < cnt/2; j++) {
+                    sb.append(ch);
                 }
             }
-            answer += sb.toString();
-            if (isOne == 1) {
-                answer += (char)(oneIdx+65);
+
+        }
+        StringBuilder reverseSb = new StringBuilder(sb).reverse();
+        String answer;
+        if (cntOdd > 1) {
+            answer = "I'm Sorry Hansoo";
+        }
+        else {
+            if (cntOdd == 1) {
+                sb.append((char)(index + 'A'));
             }
-            String end = sb.reverse().toString();
-            answer += end;
+            answer = sb.toString() + reverseSb.toString();
         }
 
         System.out.println(answer);
