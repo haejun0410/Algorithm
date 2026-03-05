a0d8d50e1b80ae54bfa304d30da44789e9dc89f4
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2026-03-05 11:19:44 +0900

    [Silver IV] Title: 좋은 단어, Time: 256 ms, Memory: 23128 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -2,34 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int n = Integer.parseInt(br.readLine());
-        
-        int count = 0;
-        for (int i=0; i<n; i++) {
-            String str = br.readLine();
-            Stack<Character> stack = new Stack<>();
+        int answer = 0;
 
-            for (int j=0; j<str.length(); j++) {
-                if (stack.isEmpty()) {
-                    stack.push(str.charAt(j));
+        for (int i = 0; i < n; i++) {
+            String word = br.readLine();
+            Deque<Character> stack = new ArrayDeque<>();
+            for (int idx = 0; idx < word.length(); idx++) {
+                char ch = word.charAt(idx);
+
+                if (stack.size() == 0 || stack.peek() != ch) {
+                    stack.push(ch);
                 }
                 else {
-                    if (stack.peek() == str.charAt(j)) {
-                        stack.pop();
-                    }
-                    else {
-                        stack.push(str.charAt(j));
-                    }
+                    stack.pop();
                 }
             }
-            if (stack.isEmpty()) {
-                count++;
-            }
+
+            answer += (stack.size() == 0 ? 1 : 0);
         }
 
-        System.out.println(count);
+        System.out.println(answer);
     }
 }
