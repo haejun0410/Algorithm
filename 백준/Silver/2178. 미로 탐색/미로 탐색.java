71ea837dabd2318c9d32841c83c828e5f904df10
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2026-03-05 13:51:04 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -6,49 +6,42 @@ public class Main {
     static int[] dy = {-1, 0, 1, 0};
     static int[] dx = {0, -1, 0, 1};
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
+        // 배열 정보
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        char[][] board = new char[n][m];
-
-        for(int i=0; i<n; i++) {
-            String arr = br.readLine();
-            for(int j=0; j<m; j++) {
-                board[i][j] = arr.charAt(j);
-            }
+        String[] board = new String[n];
+        for (int i = 0; i < n; i++) {
+            board[i] = br.readLine();
         }
+
+        int[][] visited = new int[n][m];
 
         Queue<int[]> queue = new ArrayDeque<>();
-        int[][] visited = new int[n][m];
-        for(int i=0; i<n; i++) {
-            Arrays.fill(visited[i], -1);
-        }
-
         queue.offer(new int[] {0, 0});
-        visited[0][0] = 0;
+        visited[0][0] = 1;
 
         while(!queue.isEmpty()) {
-            int[] current = queue.poll();
+            int[] curr = queue.poll();
 
-            for(int i=0; i<4; i++) {
-                int ny = current[0] + dy[i];
-                int nx = current[1] + dx[i];
+            for (int idx = 0; idx < 4; idx++) {
+                int ny = curr[0] + dy[idx];
+                int nx = curr[1] + dx[idx];
 
                 if (ny >= 0 && ny < n && nx >= 0 && nx < m) {
-                    if (visited[ny][nx] == -1 && board[ny][nx] == '1') {
+                    if (board[ny].charAt(nx) == '1' && visited[ny][nx] == 0) {
+                        visited[ny][nx] = visited[curr[0]][curr[1]] + 1;
                         queue.offer(new int[] {ny, nx});
-                        visited[ny][nx] = visited[current[0]][current[1]] + 1;
                     }
                 }
             }
+            
         }
 
-        System.out.println(visited[n-1][m-1] + 1);
-
+        System.out.println(visited[n-1][m-1]);
     }
 }
