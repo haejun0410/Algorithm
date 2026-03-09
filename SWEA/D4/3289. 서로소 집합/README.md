58df3f48ce16ef78268c39ca853291a87768da5b
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2026-03-09 11:37:20 +0900

    [D4] Title: 서로소 집합, Time: 688 ms, Memory: 118,332 KB -BaekjoonHub

---
+++ SWEA/D4/3289. 서로소 집합/README.md
@@ -0,0 +1,15 @@
+# [D4] 서로소 집합 - 3289 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWBJKA6qr2oDFAWr) 
+
+### 성능 요약
+
+메모리: 118,332 KB, 시간: 688 ms, 코드길이: 1,955 Bytes
+
+### 제출 일자
+
+2026-03-09 11:37
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+
+    static int[] parent;
+    static int[] rank;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int TC = Integer.parseInt(br.readLine());
+
+        for (int testCase = 1; testCase<=TC; testCase++) {
+            sb.append("#").append(testCase).append(" ");
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            parent = new int[n+1];
+            rank = new int[n+1];
+
+            for (int i = 0; i < n+1; i++) {
+                parent[i] = i;
+                rank[i] = 0;
+            }
+
+            for (int i = 0; i < m; i++) {
+                st = new StringTokenizer(br.readLine());
+                int command = Integer.parseInt(st.nextToken());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                if (command == 1) {
+                    sb.append(find(a) == find(b) ? 1 : 0);
+                }
+                else {
+                    union(a, b);
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static int find(int a) {
+        if (parent[a] == a) return a;
+
+        return parent[a] = find(parent[a]);
+    }
+
+    public static void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if (rootA != rootB) {
+            if (rank[rootA] < rank[rootB]) {
+                parent[rootA] = rootB;
+            }
+            else {
+                parent[rootB] = rootA;
+                if (rank[rootA] == rank[rootB]) {
+                    rank[rootA]++;
+                }
+            }
+        }
+    }
+}
