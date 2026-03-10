174795438410ca10a7c90c910abb1cfa181ba4c7
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2026-03-10 14:54:58 +0900

    [Gold II] Title: 공항, Time: 220 ms, Memory: 23196 KB -BaekjoonHub

---
+++ 백준/Gold/10775. 공항/공항.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int g = Integer.parseInt(br.readLine());
+        parent = new int[g+1];
+        for (int i = 0; i < g+1; i++) {
+            parent[i] = i;
+        }
+
+        int p = Integer.parseInt(br.readLine());
+
+        int count = 0;
+        for (int i = 0; i < p; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int root = find(n);
+
+            if (root == 0) break;
+            count++;
+            union(root, root-1);
+        }
+
+        System.out.print(count);
+    }
+
+    public static int find(int a) {
+        if (parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
+    }
+
+    public static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a != b) parent[a] = b;
+    }
+}
