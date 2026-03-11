4dcb9950692a80f2f8cc01afb26dfc5e99429153
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2026-03-11 11:29:26 +0900

    [Gold I] Title: 구간 합 구하기, Time: 540 ms, Memory: 116820 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,89 +1,120 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int N, M, K;
-    static long[] nums;
-    static long[] tree;
-    static int S;
+    public static class SegmentTree {
+        long[] tree;
+        int n;
+
+        SegmentTree(long[] arr) {
+            n = arr.length;
+            tree = new long[4*n];
+            build(arr, 1, 0, n-1);
+
+        }
+
+        // 구축
+        private void build(long[] arr, int node, int start, int end) {
+            // 리프 노드이면 -> 값 설정
+            if (start == end) {
+                tree[node] = arr[start];
+                return;
+            }
+
+            int mid = (start + end) / 2;
+            // 왼쪽 자식
+            build(arr, 2 * node, start, mid);
+            // 오른쪽 자식
+            build(arr, 2 * node + 1, mid + 1, end);
+            // 합
+            tree[node] = tree[node *2] + tree[node * 2 + 1];
+        }
+
+        // 업데이트
+        public void update(int idx, long val) {
+            updateHelper(1, 0, n-1, idx, val);
+        }
+
+        private void updateHelper(int node, int start, int end, int idx, long val) {
+            // 리프 노드이면 업데이트
+            if (start == end) {
+                tree[node] = val;
+                return;
+            }
+
+            int mid = (start + end) / 2;
+            // idx가 왼쪽 자식에 존재
+            if (idx <= mid) {
+                updateHelper(2 * node, start, mid, idx, val);
+            }
+            // idx가 오른쪽 자식에 존재
+            else {
+                updateHelper(2 * node + 1, mid + 1, end , idx, val);
+            }
+
+            // 변경된 자식을 통해 업데이트
+            tree[node] = tree[2*node] + tree[2*node+1];
+        }
+
+        // 쿼리
+        public long query(int l, int r) {
+            return queryHelper(1, 0, n-1, l, r);
+        }
+
+        private long queryHelper(int node, int start, int end, int l, int r) {
+            // 1. 구간 밖이면 0
+            if (end < l || start > r) return 0;
+            // 2. 완전히 포함되면 반환
+            if (start >= l && end <= r) return tree[node];
+            // 3. 일부만 포함되면 재귀
+            int mid = (start + end) / 2;
+            // 왼쪽 자식
+            long leftSum = queryHelper(2 * node, start, mid, l, r);
+            // 오른쪽 자식
+            long rightSum = queryHelper(2 * node + 1, mid + 1, end, l, r);
+
+            return leftSum + rightSum;
+        }
+
+
+    }
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        S = 1;
-        while (S < N) {
-            S *= 2;
+        // 수의 갯수
+        int n = Integer.parseInt(st.nextToken());
+        // 변경이 일어나는 횟수
+        int m = Integer.parseInt(st.nextToken());
+        // 구간 합을 구하는 횟수
+        int k = Integer.parseInt(st.nextToken());
+
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Long.parseLong(br.readLine());
         }
 
-        nums = new long[N];
-        tree = new long[S * 2];
+        SegmentTree segmentTree = new SegmentTree(arr);
 
-        for (int i = 0; i < N; i++) {
-            nums[i] = Long.parseLong(br.readLine());
-        }
+        for (int i = 0; i < m + k; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            long c = Long.parseLong(st.nextToken());
 
-        init();
-
-        for (int i = 0; i < M + K; i++) {
-            st = new StringTokenizer(br.readLine());
-            int type = Integer.parseInt(st.nextToken());
-
-            if (type == 1) {
-                int idx = Integer.parseInt(st.nextToken());
-                long val = Long.parseLong(st.nextToken());
-                long diff = val - tree[S + idx - 1];
-                update(1, S, 1, idx, diff);
-            } else {
-                int from = Integer.parseInt(st.nextToken());
-                int to = Integer.parseInt(st.nextToken());
-                System.out.println(query(1, S, 1, from, to));
+            if (a == 1) {
+                segmentTree.update(b-1, c);
+            }
+            else {
+                sb.append(segmentTree.query(b-1, (int)c-1)).append("\n");
             }
         }
-        br.close();
 
-    }
-
-    private static void init() {
-        for (int i = 0; i < N; i++) {
-            tree[S + i] = nums[i];
-        }
-        
-        for (int i = S - 1; i > 0; i--) {
-            tree[i] = tree[i * 2] + tree[i * 2 + 1];
-        }
-    }
-
-    static long query(int left, int right, int node, int queryLeft, int queryRight) {
-        if (queryRight < left || right < queryLeft) { 
-            return 0;
-        } else if (queryLeft <= left && right <= queryRight) { 
-            return tree[node];
-        } else { 
-            int mid = (left + right) / 2;
-            return query(left, mid, node * 2, queryLeft, queryRight)
-                    + query(mid + 1, right, node * 2 + 1, queryLeft, queryRight);
-        }
-    }
-
-    static void update(int left, int right, int node, int target, long diff) {
-
-        if (target < left || right < target) {
-            return;
-        } else { 
-            tree[node] += diff;
-            if (left != right) {
-                int mid = (left + right) / 2;
-                update(left, mid, node * 2, target, diff);
-                update(mid + 1, right, node * 2 + 1, target, diff);
-            }
-        }
+        System.out.print(sb);
     }
 }
