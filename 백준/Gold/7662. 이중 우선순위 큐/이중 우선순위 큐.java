7e18269018ee2bf0caf61cce70ddda277f5f3d7e
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2026-03-26 15:02:13 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2332 ms, Memory: 357484 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,48 +1,52 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int T = Integer.parseInt(br.readLine());
-        for (int t=0; t<T; t++) {
-            int k = Integer.parseInt(br.readLine());
-            TreeMap<Integer, Integer> map = new TreeMap<>();
-
-            for (int i=0; i<k; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                String cmd = st.nextToken();
-                int num = Integer.parseInt(st.nextToken());
-
-                if (cmd.equals("I")) {
-                    map.put(num, map.getOrDefault(num, 0) + 1);
-                } else {
-                    if (map.isEmpty()) continue;
-
-                    if (num == 1) {
-                        int max = map.lastKey();
-                        if (map.get(max) == 1) {
-                            map.remove(max);
-                        } else {
-                            map.put(max, map.get(max) - 1);
-                        }
-                    } else if (num == -1) {
-                        int min = map.firstKey();
-                        if (map.get(min) == 1) {
-                            map.remove(min);
-                        } else {
-                            map.put(min, map.get(min) - 1);
-                        }
-                    }
-                }
-            }
-
-            if (map.isEmpty()) {
-                System.out.println("EMPTY");
-            } else {
-                System.out.println(map.lastKey() + " " + map.firstKey());
-            }
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int TC = Integer.parseInt(br.readLine());
+		
+		TreeMap<Integer, Integer> map = new TreeMap<>();
+		
+		for (int testCase = 1; testCase <= TC; testCase++) {
+			int n = Integer.parseInt(br.readLine());
+			int count = 0;
+			map.clear();
+			
+			for (int i = 0 ; i < n; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+				String command = st.nextToken();
+				int num = Integer.parseInt(st.nextToken());
+				
+				if (command.equals("I")) {
+					map.put(num, map.getOrDefault(num, 0) + 1);
+					count++;
+				}
+				else {
+					if (count == 0) continue;
+					int target = (num == 1 ? map.lastKey() : map.firstKey());
+					
+					if (map.get(target) == 1) {
+						map.remove(target);
+					}
+					else {
+						map.put(target, map.get(target) -1);
+					}
+					count--;
+				}
+			}
+			
+			if (count == 0) {
+				sb.append("EMPTY").append("\n");
+			}
+			else if (count == 1) {
+				sb.append(map.lastKey()).append(" ").append(map.lastKey()).append("\n");
+			}
+			else{
+				sb.append(map.lastKey()).append(" ").append(map.firstKey()).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
 }
