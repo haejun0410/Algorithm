209128f77adf446a65a149326163a4b98e19096b
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2026-04-20 16:39:02 +0900

    [level 3] Title: 섬 연결하기, Time: 1.25 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,39 +1,56 @@
 import java.util.*;
 
 class Solution {
-    int[] parent;
-    
-    public int find(int a) {
-        if (parent[a] == a) return a;
-        return parent[a] = find(parent[a]);
-    }
-    
-    public void union(int a, int b) {
-        int rootA = find(a);
-        int rootB = find(b);
-        if (rootA != rootB) {
-            parent[rootB] = rootA;
+    class Edge implements Comparable<Edge> {
+        int node;
+        int dist;
+        
+        Edge(int node, int dist) {
+            this.node = node;
+            this.dist = dist;
+        }
+        
+        @Override
+        public int compareTo(Edge o) {
+            return this.dist - o.dist;
         }
     }
     
     public int solution(int n, int[][] costs) {
-        int totalCost = 0;
         
-        parent = new int[n];
+        List<List<Edge>> adj = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            parent[i] = i;
+            adj.add(new ArrayList<>());
         }
         
-        Arrays.sort(costs, (o1, o2) -> Integer.compare(o1[2], o2[2]));
-        
         for (int[] cost : costs) {
-            int from = cost[0];
-            int to = cost[1];
-            int weight = cost[2];
+            adj.get(cost[0]).add(new Edge(cost[1], cost[2]));
+            adj.get(cost[1]).add(new Edge(cost[0], cost[2]));
+        }
+        
+        boolean[] visited = new boolean[n];
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        
+        pq.add(new Edge(0, 0));
+        
+        int totalCost = 0;
+        int edgeCount = 0;
+        
+        while (!pq.isEmpty()) {
+            Edge current = pq.poll();
             
-            if (find(from) != find(to)) {
-                union(from, to);
-                totalCost += weight;
+            if (visited[current.node]) continue;
+            
+            visited[current.node] = true;
+            totalCost += current.dist;
+            edgeCount++;
+            
+            if (edgeCount == n) break;
+            
+            for (Edge next : adj.get(current.node)) {
+                if (!visited[next.node]) {
+                    pq.add(next);
+                }
             }
         }
         
