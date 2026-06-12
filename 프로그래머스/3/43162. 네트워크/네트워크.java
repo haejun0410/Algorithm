72fc2ca1e723c3390d4f129c86943e4b7c02b905
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2026-06-12 13:15:18 +0900

    [level 3] Title: 네트워크, Time: 0.23 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,22 +1,21 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
-    
     public int solution(int n, int[][] computers) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        boolean[] visited = new boolean[computers[0].length];
-        
         int count = 0;
+        boolean[] visited = new boolean[n];
         
-        for(int i=0; i<computers[0].length; i++) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                visited[i] = true;
                 count++;
+                visited[i] = true;
                 queue.offer(i);
                 while(!queue.isEmpty()) {
-                    int current = queue.poll();
-                    for(int j=0; j<computers[0].length; j++) {
-                        if (j != current && computers[current][j] == 1 && !visited[j]) {
+                    int curr = queue.poll();
+                    for (int j = 0; j < n; j++) {
+                        if (computers[curr][j] == 1 && !visited[j]) {
                             visited[j] = true;
                             queue.offer(j);
                         }
@@ -24,7 +23,6 @@ class Solution {
                 }
             }
         }
-        
         return count;
     }
 }
