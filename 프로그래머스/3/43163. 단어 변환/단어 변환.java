272def8cbaddff4d07ae86c17911d3707d949bb4
LeeHaeJun <50326082+haejun0410@users.noreply.github.com>
2026-06-15 10:07:03 +0900

    [level 3] Title: 단어 변환, Time: 0.58 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,42 +1,77 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        Queue<String> queue = new ArrayDeque<>();
-        queue.offer(begin);
-        boolean[] visited = new boolean[words.length];
+        int n = words.length;
+        int wordLen = words[0].length();
         
-        int step = 0;
+        int targetIndex = -1;
+        for (int i = 0; i < n; i++) {
+            if (words[i].equals(target)) {
+                targetIndex = i;
+                break;
+            }
+        }
+        if (targetIndex == -1) return 0;
         
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-            for(int k = 0; k < size; k++) {
-                String curr = queue.poll();
-                if (curr.equals(target)) {
-                    return step;
-                }
-
-                for (int i = 0; i < words.length; i++) {
-                    if (!visited[i]) {
-                        int diffCount = 0;
-                        for (int idx = 0; idx < curr.length(); idx++) {
-                            if (curr.charAt(idx) != words[i].charAt(idx)) {
-                                diffCount++;
-                            }
-                        }
-
-                        if (diffCount == 1) {
-                            queue.offer(words[i]);
-                            visited[i] = true;
-                        }
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                String word1 = words[i];
+                String word2 = words[j];
+                int count = 0;
+                for (int k = 0; k < wordLen; k++) {
+                    if (word1.charAt(k) == word2.charAt(k)) {
+                        count++;
                     }
                 }
+                
+                if (count == wordLen -1) {
+                    adj.get(j).add(i);
+                    adj.get(i).add(j);
+                }
+            }
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        int[] dist = new int[n];
+        Arrays.fill(dist, -1);
+        
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int k = 0; k < wordLen; k++) {
+                if (begin.charAt(k) == words[i].charAt(k)) {
+                    count++;
+                }
+            }
+            if (count == wordLen - 1) {
+                queue.offer(i);
+                dist[i] = 1;
+            }
+        }
+        
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            
+            if (curr == targetIndex) {
+                return dist[curr];
             }
             
-            step++;
+            for (int next : adj.get(curr)) {
+                if (dist[next] == -1) {
+                    dist[next] = dist[curr] + 1;
+                    queue.offer(next);
+                }
+            }
         }
         
         return 0;
+        
         
     }
 }
